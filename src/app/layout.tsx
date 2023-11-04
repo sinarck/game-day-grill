b0f19dab4f98ca087a95cc6bc2e31e8a6954de36
Aadi Sanghvi <aadis@duck.com>
2023-11-04 11:31:17 -0500

    improvement: modular metadata through siteConfig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,36 @@
-import "@/styles/globals.css"
-
+import { Metadata } from "next"
 import { Inter } from "next/font/google"
-
 import { ThemeProvider } from "@/components/theme-provider"
+import { siteConfig } from "@/config/site"
+import "@/styles/globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
-  title: "Game Day Grill",
-  description: "A project for BPA",
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      type: "image/ico",
+    },
+    shortcut: { url: "/favicon.png", type: "image/png" },
+    apple: "/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>
---
+++ src/config/site.ts
@@ -0,0 +1,26 @@
+export const siteConfig = {
+  name: "Game Day Grill",
+  description: "BPA Website Design Team 2023",
+  mainNav: [
+    {
+      title: "Technology",
+      href: "/tech",
+    },
+    {
+      title: "Ethics",
+      href: "/ethics",
+    },
+    {
+      title: "Applications",
+      href: "/applications",
+    },
+
+    {
+      title: "Sources",
+      href: "/sources",
+    },
+  ],
+  links: {
+    github: "https://github.com/aadisanghvii/gt-hsa",
+  },
+}
