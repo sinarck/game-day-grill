e6b86de2318dc9b6b45ce0670b8f72465b0db0c7
Aadi Sanghvi <aadis@duck.com>
2023-11-12 20:50:54 -0600

    chore: remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { SiteHeader } from "@/components/site-header"
-import { ThemeToggle } from "@/components/theme-toggle"
+import { SiteHeader } from "@/components/site-header";
 
 export default function Home() {
   return (
     <main className="m-0 h-full min-h-full flex items-center justify-center">
       <SiteHeader />
     </main>
-  )
+  );
 }
