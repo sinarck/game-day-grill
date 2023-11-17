26c1433346d3781e56e6f6266c7940b14670c6af
Aadi Sanghvi <aadis@duck.com>
2023-11-17 03:20:52 -0600

    chore: remove theme variance and buggy shadcn form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import SignUp from '@/components/form/SignUp';
+"use client";
+import SignUp from "@/components/form/SignUp";
 import { SiteHeader } from "@/components/site-header";
 
 export default function Home() {
   return (
     <main className="m-0 h-full min-h-full flex items-center justify-center">
-      {/* <SiteHeader /> */}
-      <SignUp />
+      <SiteHeader />
+      {/* <SignUp /> */}
     </main>
   );
 }
--- src/components/form/SignUp.tsx
+++ src/components/form/SignUp.tsx
@@ -1,122 +1,43 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-import { Button } from "@/components/ui/button";
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 
-import * as z from 'zod';
+import * as z from "zod";
 
 const formSchema = z
-    .object({
-        username: z.string().min(1, 'Username is required').max(100),
-        email: z.string().min(1, 'Email is required').email('Invalid email'),
-        password: z
-            .string()
-            .min(1, 'Password is required')
-            .min(8, 'Password must have than 8 characters'),
-        confirmPassword: z.string().min(1, 'Password confirmation is required'),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-        path: ['confirmPassword'],
-        message: 'Password do not match',
-    });
+  .object({
+    username: z.string().min(1, "Username is required").max(100),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have than 8 characters"),
+    confirmPassword: z.string().min(1, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password do not match",
+  });
 
 const SignUp = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        defaultValues: {
-            username: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-        }
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-    };
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="test@example.com" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
-    );
+  return (
+    <></>
+  );
 };
 
 export default SignUp;
