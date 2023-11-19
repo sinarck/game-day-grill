8d47c78f66670db964fd9200b33d3e3e43a11765
sinarck <j7le2w3f@duck.com>
2023-11-19 12:27:30 -0600

    chore: refactor forms and buttons to components

--- src/app/(info)/signup/page.tsx
+++ src/app/(info)/signup/page.tsx
@@ -5,13 +5,10 @@ import { motion } from "framer-motion"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 
-import Input from "@/components/form/input"
+import Form from "@/components/form/form"
 import { enrollSchema } from "@/schema/form"
 import { enrollForm } from "@/types/auth"
 import { useRouter } from "next/navigation"
-import { Loader2 } from "lucide-react"
-import { Icons } from "@/components/icons"
-import { cn } from "@/lib/merge"
 
 const Page = () => {
   const [loading, setLoading] = useState(false)
@@ -53,40 +50,20 @@ const Page = () => {
   })
 
   return (
-    <div className="flex items-center justify-center h-screen overflow-hidden">
-      <div className="align-middle justify-center items-center flex flex-col overscroll-none bg-gray-100 rounded-xl max-h-80 w-50 p-10 shadow-lg">
-        <h1 className="mb-4 font-bold text-lg">Ready to join the family?</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col">
-            <Input
-              fieldName="username"
-              labelName="Username"
-              type=""
-              errors={errors}
-              register={register}
-            />
-            <Input
-              fieldName="password"
-              labelName="Password"
-              type="password"
-              errors={errors}
-              register={register}
-            />
-          </div>
-          <div className="pt-8">
-            <button
-              type="submit"
-              className={cn(
-                "rounded-md bg-black text-white p-2 px-8 flex gap-2 items-center align-middle justify-center ",
-                loading && "px-[19px]"
-              )}
-            >
-              {loading && <Icons.spinner />}
-              <p>Create Account</p>
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className="flex items-center justify-center h-screen">
+      <motion.div
+        className="align-middle justify-center items-center flex flex-col overscroll-none bg-gray-100 rounded-xl max-h-80 w-50 p-10 shadow-lg"
+        // animate={{ y: -100 }}
+      >
+        <Form
+          errors={errors}
+          handleSubmit={handleSubmit}
+          loading={loading}
+          heading="Ready to join the family?"
+          onSubmit={onSubmit}
+          register={register}
+        />
+      </motion.div>
     </div>
   )
 }
