06f075a1dcb2b58bb6b73208b225b9fe9f0424ec
sinarck <j7le2w3f@duck.com>
2023-11-18 20:49:59 -0600

    feat: improved signup/enroll ui

--- src/schema/form.ts
+++ src/schema/form.ts
@@ -1,18 +1,12 @@
 import { z } from "zod"
 
-export const enrollSchema = z
-  .object({
-    username: z.string().min(1, "Username is required").max(100),
-    password: z
-      .string()
-      .min(1, "Password is required")
-      .min(8, "Password must have than 8 characters"),
-    confirmPassword: z.string().min(1, "Password confirmation is required"),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    path: ["confirmPassword"],
-    message: "Password do not match",
-  })
+export const enrollSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+})
 
 export const loginSchema = z.object({
   username: z.string().min(1, "Username is required").max(100),
