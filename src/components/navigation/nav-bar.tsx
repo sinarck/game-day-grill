33fce5c2f86eab9eb9b972138f6bb36c6d913a54
sinarck <j7le2w3f@duck.com>
2023-11-20 22:16:07 -0600

    chore: cleaner log in code

--- src/components/navigation/nav-bar.tsx
+++ src/components/navigation/nav-bar.tsx
@@ -2,9 +2,10 @@
 
 import NavLinks from "@/components/navigation/nav-menu"
 import { siteConfig } from "@/config/site"
-import { signOut, useSession } from "next-auth/react"
+import { useSession } from "next-auth/react"
 import Link from "next/link"
 import Button from "../ui/button"
+import Profile from "./profile"
 
 const NavBar = () => {
   const { data: session, status } = useSession()
@@ -14,31 +15,18 @@ const NavBar = () => {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <NavLinks items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4 pr-6">
-          <Button
-            loading={false}
-            type="button"
-            variant="outline"
-            onClick={
-              status === "authenticated"
-                ? () => {
-                    signOut()
-                  }
-                : () => {}
-            }
-          >
-            {status === "authenticated" ? (
-              <p className="text-lg font-semibold text-muted-foreground sm:text-sm">
-                Log out
-              </p>
-            ) : (
+          {status === "authenticated" ? (
+            <Profile />
+          ) : (
+            <Button loading={false} type="button" variant="outline">
               <Link
                 href="/login"
                 className="flex items-center text-lg font-semibold text-muted-foreground sm:text-sm"
               >
                 Log in
               </Link>
-            )}
-          </Button>
+            </Button>
+          )}
         </div>
       </div>
     </header>
