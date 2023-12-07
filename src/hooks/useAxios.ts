6918791d1643779abfddd59c796a06c68eb1c335
sinarck <j7le2w3f@duck.com>
2023-12-06 18:59:08 -0600

    chore: remove callback hook from useAxios

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,7 +2,7 @@ import { ToastProps } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
 import axios, { AxiosResponse } from "axios"
 import { SignInResponse } from "next-auth/react"
-import { useCallback, useState } from "react"
+import { useState } from "react"
 
 interface FetchProps<T> {
   endpoint: string
@@ -22,43 +22,44 @@ const useAxios = <T = any>() => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [loading, setLoading] = useState(false)
 
-  const fetch = useCallback(
-    async <K>({ endpoint, body, callbackFunction }: FetchProps<K>) => {
-      const controller = new AbortController()
-      setLoading(true)
-      setError(false)
-      setErrorMessage(null)
+  const fetch = async <K>({
+    endpoint,
+    body,
+    callbackFunction,
+  }: FetchProps<K>) => {
+    const controller = new AbortController()
+    setLoading(true)
+    setError(false)
+    setErrorMessage(null)
 
-      for (let i = 0; i < 3; i++) {
-        // Retry up to 3 times
-        try {
-          const res = await axios.post(endpoint, body, {
-            signal: controller.signal,
-          })
+    for (let i = 0; i < 3; i++) {
+      // Retry up to 3 times
+      try {
+        const res = await axios.post(endpoint, body, {
+          signal: controller.signal,
+        })
 
-          setResponse(res)
+        setResponse(res)
 
-          if (callbackFunction) {
-            await callbackFunction
-          }
-
-          // If the request was successful, break the loop
-          break
-        } catch (err) {
-          // If this was the last retry, throw the error
-          if (i === 2) {
-            setError(true)
-            setErrorMessage(err.response.data.message)
-          } else {
-            await new Promise((resolve) => setTimeout(resolve, 1000)) // Wait for 1 second before retrying
-          }
-        } finally {
-          setLoading(false)
+        if (callbackFunction) {
+          await callbackFunction
         }
+
+        // If the request was successful, break the loop
+        break
+      } catch (err) {
+        // If this was the last retry, throw the error
+        if (i === 2) {
+          setError(true)
+          setErrorMessage(err.response.data.message)
+        } else {
+          await new Promise((resolve) => setTimeout(resolve, 1000)) // Wait for 1 second before retrying
+        }
+      } finally {
+        setLoading(false)
       }
-    },
-    []
-  )
+    }
+  }
 
   return {
     fetch: fetch,
